e543578c7203739f698ccd5334a0a8bc54d53be3
Dilan Chirva <chirvadilan82@gmail.com>
2026-03-05 14:53:54 -0500

    Primer commit

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+print("=== CALCULADORA BASICA ===")
+
+while True:
+    numero1 = float(input("Ingresa el primer numero: "))
+    numero2 = float(input("Ingresa el segundo numero: "))
+
+    operacion = input("Elige la operacion (+, -, *, /): ")
+
+    if operacion == "+":
+        resultado = numero1 + numero2
+        print("El resultado es:", resultado)
+
+    elif operacion == "-":
+        resultado = numero1 - numero2 
+        print("El resultado es:", resultado)
+
+    elif operacion == "*":
+        resultado = numero1 * numero2
+        print("El resultado es:", resultado)
+
+    elif operacion == "/":
+        if numero2 != 0:
+            resultado = numero1 / numero2
+            print("El resultado es:", resultado) 
+        else: 
+            print("Error: No se puede dividir entre cero")
+
+    else: 
+        print("Operacion no valida. Por favor, elige una operacion correcta.")
